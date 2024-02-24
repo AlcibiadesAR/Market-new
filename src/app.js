f57e7d5fcbde7067732d836528579e9b179e1606
AlcibiadesAR <alcibiadesacosta99@gmail.com>
2024-02-23 22:16:09 -0600

    modificaciones

--- src/app.js
+++ src/app.js
@@ -2,15 +2,18 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const publicPath = path.resolve(__dirname, '../public')
-app.use(express.static(publicPath))
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
 
-const port = process.env.PORT|| 3002
-app.listen(port, function (){
+const port = process.env.PORT || 3002;
+app.listen(port, function () {
     console.log('Servidor Levantado');
-})
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/index.html'))
-})
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
